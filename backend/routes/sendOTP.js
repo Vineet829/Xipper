af3a3efe8aa9f265403bfc76fc9e54958fb5f187
Vineet829 <vineetsingh5987@gmail.com>
2024-09-17 02:03:40 +0530

    feat: add OTP sending endpoint

--- backend/routes/sendOTP.js
+++ backend/routes/sendOTP.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const twilio = require('twilio');
 require('dotenv').config();
 
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const serviceSid = process.env.TWILIO_SERVICE_SID;
